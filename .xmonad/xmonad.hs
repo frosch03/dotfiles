32eb1649821cbbeb9b1a5d51956a9cc3acd3738c
Matthias Brettschneider <frosch03@frosch03.de>
2017-03-04 20:27:33 +0100

    removed unused sections from configuration

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -20,7 +20,6 @@ import XMonad.Layout.Reflect
 import XMonad.Layout.Named
 import XMonad.Layout.Circle
 import XMonad.Layout.Gaps
---import XMonad.Layout.StackTile
 
 import XMonad.Prompt
 import XMonad.Prompt.Shell
@@ -35,11 +34,7 @@ import System.IO
 import Data.Ratio ((%))
 import qualified Data.Map as M
 
--- for xmobarStrip
 import Data.List (isPrefixOf)
-
--- for dzen2
---import XMonad.Util.Dzen
 
 
 wmName       = stringProperty "WM_NAME"
@@ -69,8 +64,8 @@ main = do
          spawnPipe "/home/frosch03/bin/xStartup"
          xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
             { borderWidth        = 1
-            , normalBorderColor  = "#333333" -- previous "#003300"
-            , focusedBorderColor = "#999999" -- previous "#00ff00"
+            , normalBorderColor  = "#333333"
+            , focusedBorderColor = "#999999"
             , modMask            = mod4Mask
             , keys               = newKeys
             , workspaces         = myWorkspaces
@@ -81,12 +76,12 @@ main = do
 
 -- The pretty printed layout for my lemonbar 
 myLemonbarPP = defaultPP 
-                 { ppTitle   =  ("WIN" ++) -- (\x -> (wrap "WIN" "") x) -- . shorten 50 
-                 , ppCurrent =  ("FOC" ++) -- (\x -> (wrap "FOC" "") x) -- . wrap "[" "]" 
-                 , ppVisible =  ("ACT" ++) -- (\x -> (wrap "ACT" "") x) -- . wrap "(" ")"
-                 , ppUrgent  =  ("URG" ++) -- (\x -> (wrap "URG" "") x) -- . xmobarStrip
-                 , ppHidden  =  ("INA" ++) -- (\x -> (wrap "INA" "") x) --
-		 , ppLayout  =  ("LAY" ++) -- (\x -> (wrap "LAY" "") x)
+                 { ppTitle   =  ("WIN" ++)
+                 , ppCurrent =  ("FOC" ++)
+                 , ppVisible =  ("ACT" ++)
+                 , ppUrgent  =  ("URG" ++)
+                 , ppHidden  =  ("INA" ++)
+		 , ppLayout  =  ("LAY" ++)
                  , ppSep     = "\n"
 		 , ppWsSep   = " "
 		 , ppOutput  = \x -> writeFile "/tmp/xmonad_lemonbar_frosch03" $ "WSP" ++ x ++ "\n"
@@ -95,7 +90,6 @@ myLemonbarPP = defaultPP
 
 -- The layoutdefinition for my workspaces
 myLayout  = smartBorders
---	  $ gaps [(U,14)] -- hard coded gap of 14 pixels at the top, to fit the lemonbar there
           $ onWorkspaces ["1"]      (Full ||| tiled ||| Mirror tiled)
           $ onWorkspaces ["code"]   (codeColumn ||| codeRow)
           $ onWorkspaces ["web"]    (Full ||| (gaps [(L,250), (R,250)] $ Full))
@@ -103,7 +97,6 @@ myLayout  = smartBorders
           $ onWorkspaces ["im"]     (   reflectHoriz (pidginLayout gridLayout)
                                     ||| reflectHoriz (pidginLayout Full)
                                     ||| reflectHoriz (pidginLayout Circle)
---                                    ||| reflectHoriz (pidginLayout (StackTile 1 (10/100) (90/100)))
                                     )
 	  $ onWorkspaces ["mail"]   (Full)
 	  $ onWorkspaces ["skype"]  (reflectHoriz $ (withIM (1%6) (Role "MainWindow") Grid))
@@ -127,9 +120,8 @@ myLayout  = smartBorders
 -- The XPConfig definition for my XMonad.Prompts 
 myXPConfig = defaultXPConfig { bgColor     = "black"
                              , fgColor     = "white"
-                             , borderColor = "#333333" -- previous "green"
+                             , borderColor = "#333333"
                              , position    = Top
-                             -- , font        = "xft:SourceCodePro-Regular:pixelsize=10:autohint=true"
                              , font        = "xft:Inconsolata:pixelsize=10:antialias=true:autohint=true"
                              }
 
@@ -167,10 +159,6 @@ myKeys x = M.fromList $
   , ((0        ,          xK_XF86Thnk), spawn "/home/frosch03/whatIsThisPlace")
   , ((0        ,         xK_XF86Sleep),	spawn "sudo pm-suspend")
   , ((0        ,   xK_XF86ScreenSaver),	spawn "gnome-screensaver-command --lock")
-  -- , ((0      ,xK_XF86AudioRaiseVolume),	spawn "/home/frosch03/.xmonad/volUp.sh")
-  -- , ((0      ,xK_XF86AudioLowerVolume),	spawn "/home/frosch03/.xmonad/volDn.sh")
-  -- , ((0      ,xK_XF86AudioRaiseVolume),	spawn "pacmd set-sink-volume 0 $(printf '0x%x' $(( $(pacmd dump|grep set-sink-volume|cut -f3 -d' ') + 0xf00)) )")
-  -- , ((0      ,xK_XF86AudioLowerVolume),	spawn "pacmd set-sink-volume 0 $(printf '0x%x' $(( $(pacmd dump|grep set-sink-volume|cut -f3 -d' ') - 0xf00)) )")
   ]
 newKeys x = myKeys x `M.union` keys defaultConfig x
 
@@ -189,58 +177,4 @@ myManagedHook = composeAll . concat $
   , [ className =? x --> doF (S.shift "skype") | x <- skypeApp ]
   , [ title     =? x --> doF (S.shift "irc")   | x <- ircApps ]
   , [ isFullscreen   --> doFullFloat]
-  -- , [ resource  =? "lemonbar" --> doIgnore
-  --   , className =? "lemonbar" --> doIgnore
-  --   , title     =? "lemonbar" --> doIgnore
-  --   , resource  =? "bar"      --> doIgnore
-  --   , className =? "bar"      --> doIgnore
-  --   , title     =? "bar"      --> doIgnore
-  --   , manageDocks
-  --   ]
   ]
-
-
---
--- out of my way :)
---
-
-
-
--- The pretty printed layout for my xmobar 
-myXmobarPP = defaultPP 
-                 { ppTitle   = xmobarColor "#6080ff" "" . shorten 50 
-                 , ppCurrent = xmobarColor "#ffffff" "" . wrap "[" "]" 
-                 , ppVisible = xmobarColor "#999999" "" . wrap "(" ")"
-                 , ppUrgent  = xmobarColor "#c04040" "" . xmobarStrip
-                 , ppHidden  = xmobarColor "#000000" ""
-                 , ppSep     = " | "
-                 } 
-                 
-myDzen2PP = defaultPP 
-                 { ppTitle   = dzenColor "#61ce3c" "" . dzenEscape
-                 , ppCurrent = dzenColor "#f8f8f8" "DodgerBlue4" . pad
-                 , ppVisible = dzenColor "#f8f8f8" "LightSkyBlue4" . pad
-                 , ppUrgent  = dzenColor "#f8f8f8" "red4" . pad . dzenStrip
-                 , ppHidden  = pad
-                 , ppSep     = " "
-                 , ppWsSep   = "|"
---               , ppLayout  = dzenColor "DarkOrange" "" . wrap "^ca(1,xdotool key alt+space)[" "]^ca()"
-                 , ppLayout  = (wrap "^ca(1,xdotool key alt+space)^fg(darkorange)|^fg(grey)" "^fg(darkorange)|^ca()") . 
-                               (\x -> case x of
-                                "Tall"                -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_tall.xbm)"
-                                "Mirror Tall"         -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_mirror_tall.xbm)"
-                                "Full"                -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_full.xbm)"
-                                "Row Code"            -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_row_code.xbm)"
-                                "Column Code"         -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_column_code.xbm)"
-                                "ReflectX IM Grid"    -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_reflectx_im_grid.xbm)"
-                                "IM ReflectX IM Full" -> "^i(/home/frosch03/.dzen2/dzenIcons/xbm8x8/layout_im_reflectx_im_full.xbm)"
-                               )
-
-                 }
-              --    (\x -> case x of
-              --       "Tall" -> "^i(/home/dk/.icons/dzen2/tall.xbm)"
-              --        "Mirror Tall" -> "^i(/home/dk/.icons/dzen2/mtall.xbm)"
-              --        "Full" -> "^i(/home/dk/.icons/dzen2/full.xbm)"
-              --    )
-
-
